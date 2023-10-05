6f151a7eb2961e8bde4ef3feb6dcf3092b353d3c
krishna <krishnajj99@gmail.com>
2023-10-05 20:36:20 +0530

    Using Signals to update a total price

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-signals';
+  items = [{ name: 'Product A', price: 10 }, { name: 'Product B', price: 15 }, { name: 'Product C', price: 20 }];
+
+  itemList = signal(this.items);
+
+  totalPrice = computed(() => {
+    return this.itemList().reduce((acc, curr) => acc + curr.price, 0);
+  });
+
+  removeItem(item: any) {
+    // Update the itemList signal by removing the selected item
+    this.itemList.set(this.itemList().filter((i) => i !== item));
+  }
 }
